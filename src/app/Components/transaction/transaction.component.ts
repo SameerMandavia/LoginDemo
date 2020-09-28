28bbb43124e186f4a13d9d6b1a77c65205c7b5d1
SameerMandavia <60601676+SameerMandavia@users.noreply.github.com>
2020-09-28 16:31:12 +0530

    Spinner added at Login

---
+++ src/app/Components/transaction/transaction.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Order } from 'src/app/Models/order';
+import { Transaction } from '../../Models/transaction';
+import { User } from 'src/app/Models/user';
+import { Router } from '@angular/router';
+import { TransactionService } from '../../Services/transaction.service';
+import { LoginService } from '../../Services/login.service';
+import { DataService } from '../../Services/data.service';
+
+/**
+ * @author Sameer Mandavia
+ * Transaction Component to complete User's order payment.
+ */
+
+@Component({
+  selector: 'app-transaction',
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.css'],
+})
+export class TransactionComponent implements OnInit {
+  /**
+   * @param order       -> To store order details (Product Info,User Info,Total Amount,Total Quantity).
+   * @param transaction -> To store order details and Transaction.
+   * @param user        -> To store User info using session.
+   * @param orderInfo   ->  In DataService, I've created getter & setter method for get the current,
+   *                      product Info, And it will store the details in orderInfo Object to buy
+   *                      a product.
+   */
+
+  public order: Order;
+  public transaction: Transaction;
+  public user: User;
+  public orderInfo: Order;
+
+  //Injected Services and router for navigation.
+  constructor(
+    private transactionService: TransactionService,
+    private loginService: LoginService,
+    private dataService: DataService,
+    private router: Router
+  ) {
+    this.order = new Order();
+    this.transaction = new Transaction();
+    this.orderInfo = new Order();
+  }
+
+  //First get User details by user session and Validations for Transaction form.
+  ngOnInit(): void {
+    this.user = this.loginService.getUserSession();
+    console.log(this.user);
+
+    this.orderInfo = this.dataService.getProductInfo();
+    console.log('order info', this.orderInfo);
+  }
+
+  //Method for buy a Product. In which order details has to assign in Transaction Variable.
+  buyProduct() {
+    this.transaction.order = this.orderInfo;
+    this.transaction.user= this.orderInfo.user;
+    this.transactionService
+      .addTransaction(this.transaction)
+      .subscribe((payment) => {
+        console.log(payment, 'Transaction');
+
+        alert('Payment successful!! Thank you for ordering!');
+      });
+    this.transactionService.saveOrder(this.orderInfo).subscribe((order) => {
+      console.log((this.order = order));
+    });
+
+    console.log(this.transaction);
+    this.router.navigate(['products']);
+  }
+}
